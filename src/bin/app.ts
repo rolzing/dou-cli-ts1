d224efd9b0dff042fd3c78c562d7e5c245c11e23
Ricardo Lopez Hernandez <rolzing.richardo@gmail.com>
2018-08-24 10:28:09 -0500

    add base project

---
+++ src/bin/app.ts
@@ -0,0 +1,28 @@
+import * as commander from 'commander';
+import chalk from "chalk";
+const figlet = require('figlet');
+
+
+export class App {
+
+    private program: commander.CommanderStatic;
+    private package: any;
+
+    constructor() {
+        this.program = commander;
+        this.package = require('../../package.json');
+    }
+
+    public initialize() {
+        process.stdout.write(chalk.blue(figlet.textSync('dOu')));
+
+        this.program
+            .version(this.package.version)
+            .command('write [message]', 'say hello!')
+            .parse(process.argv);
+    }
+
+}
+
+let app = new App();
+app.initialize();
